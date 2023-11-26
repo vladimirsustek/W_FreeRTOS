7f6e071c27002bbcd977c2d2386a20acb5b1a4ae
vlasus <sustek.vladimir.ml@gmail.com>
2023-11-26 12:50:56 +0100

    Re-struct

---
+++ static_services.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <intrin.h>
+
+/* Standard includes. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+/* FreeRTOS kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Standard demo includes. */
+#include "StaticAllocation.h"
+
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char* pcTaskName);
+
+/*-----------------------------------------------------------*/
+
+void vAssertCalled(unsigned long ulLine, const char* const pcFileName);
+
+/*-----------------------------------------------------------*/
+
+/* configUSE_STATIC_ALLOCATION is set to 1, so the application must provide an
+implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
+used by the Idle task. */
+void vApplicationGetIdleTaskMemory(StaticTask_t** ppxIdleTaskTCBBuffer, StackType_t** ppxIdleTaskStackBuffer, uint32_t* pulIdleTaskStackSize);
+
+/*-----------------------------------------------------------*/
+
+/* configUSE_STATIC_ALLOCATION and configUSE_TIMERS are both set to 1, so the
+application must provide an implementation of vApplicationGetTimerTaskMemory()
+to provide the memory that is used by the Timer service task. */
+void vApplicationGetTimerTaskMemory(StaticTask_t** ppxTimerTaskTCBBuffer, StackType_t** ppxTimerTaskStackBuffer, uint32_t* pulTimerTaskStackSize);
